daa1f28685ce3bafe305e058bcf9c7f533d06019
lfhan <heb_sjz_hlf@163.com>
2017-09-12 15:44:30 +0800

    1

--- src/main/java/tmd/T.java
+++ src/main/java/tmd/T.java
@@ -2,6 +2,19 @@ package tmd;
 
 public class T {
 	public static void main(String[] args) {
-		System.out.println(1111);
+//		System.out.println(1111);
+		
+//		String s = "s";
+//		StringBuffer sb = new StringBuffer("t");
+//		System.out.println(s.hashCode());
+//		System.out.println(sb.hashCode());
+		Object[] elementData = {};
+		Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+		int minCapacity = 1;
+		int DEFAULT_CAPACITY = 10;
+		if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            minCapacity = Math.max(DEFAULT_CAPACITY, minCapacity);
+        }
+		System.out.println(minCapacity);
 	}
 }
